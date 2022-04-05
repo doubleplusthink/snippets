a1751eeb98a1f52fdb590001f4ca0a3b8aedce25
Richard Sellers <44386484+doubleplusthink@users.noreply.github.com>
2022-04-05 14:05:07 -0600

    Create script.groovy

---
+++ widget/script.groovy
@@ -0,0 +1,44 @@
+_object.cfA1Actual = _request.getParameter("data(cfA1Actual)") ? _request.getParameter("data(cfA1Actual)").toDouble() : null
+_object.cfA2Actual = _request.getParameter("data(cfA2Actual)") ? _request.getParameter("data(cfA2Actual)").toDouble() : null
+_object.cfA3Actual = _request.getParameter("data(cfA3Actual)") ? _request.getParameter("data(cfA3Actual)").toDouble() : null
+_object.cfA4Actual = _request.getParameter("data(cfA4Actual)") ? _request.getParameter("data(cfA4Actual)").toDouble() : null
+_object.cfA5Actual = _request.getParameter("data(cfA5Actual)") ? _request.getParameter("data(cfA5Actual)").toDouble() : null
+_object.cfA6Actual = _request.getParameter("data(cfA6Actual)") ? _request.getParameter("data(cfA6Actual)").toDouble() : null
+_object.cfA7Actual = _request.getParameter("data(cfA7Actual)") ? _request.getParameter("data(cfA7Actual)").toDouble() : null
+_object.cfA8Actual = _request.getParameter("data(cfA8Actual)") ? _request.getParameter("data(cfA8Actual)").toDouble() : null
+_object.cfA9Actual = _request.getParameter("data(cfA9Actual)") ? _request.getParameter("data(cfA9Actual)").toDouble() : null
+_object.cfA10Actual = _request.getParameter("data(cfA10Actual)") ? _request.getParameter("data(cfA10Actual)").toDouble() : null
+_object.cfA11Actual = _request.getParameter("data(cfA11Actual)") ? _request.getParameter("data(cfA11Actual)").toDouble() : null
+
+_object.cfA1Legal = _request.getParameter("data(cfA1Legal)") ? _request.getParameter("data(cfA1Legal)").toDouble() : null
+_object.cfA2Legal = _request.getParameter("data(cfA2Legal)") ? _request.getParameter("data(cfA2Legal)").toDouble() : null
+_object.cfA3Legal = _request.getParameter("data(cfA3Legal)") ? _request.getParameter("data(cfA3Legal)").toDouble() : null
+_object.cfA4Legal = _request.getParameter("data(cfA4Legal)") ? _request.getParameter("data(cfA4Legal)").toDouble() : null
+_object.cfA5Legal = _request.getParameter("data(cfA5Legal)") ? _request.getParameter("data(cfA5Legal)").toDouble() : null
+_object.cfA6Legal = _request.getParameter("data(cfA6Legal)") ? _request.getParameter("data(cfA6Legal)").toDouble() : null
+_object.cfA7Legal = _request.getParameter("data(cfA7Legal)") ? _request.getParameter("data(cfA7Legal)").toDouble() : null
+_object.cfA8Legal = _request.getParameter("data(cfA8Legal)") ? _request.getParameter("data(cfA8Legal)").toDouble() : null
+_object.cfA9Legal = _request.getParameter("data(cfA9Legal)") ? _request.getParameter("data(cfA9Legal)").toDouble() : null
+_object.cfA10Legal = _request.getParameter("data(cfA10Legal)") ? _request.getParameter("data(cfA10Legal)").toDouble() : null
+_object.cfA11Legal = _request.getParameter("data(cfA11Legal)") ? _request.getParameter("data(cfA11Legal)").toDouble() : null
+
+_object.cfA1Over = _request.getParameter("data(cfA1Over)") ? _request.getParameter("data(cfA1Over)").toDouble() : null
+_object.cfA2Over = _request.getParameter("data(cfA2Over)") ? _request.getParameter("data(cfA2Over)").toDouble() : null
+_object.cfA3Over = _request.getParameter("data(cfA3Over)") ? _request.getParameter("data(cfA3Over)").toDouble() : null
+_object.cfA4Over = _request.getParameter("data(cfA4Over)") ? _request.getParameter("data(cfA4Over)").toDouble() : null
+_object.cfA5Over = _request.getParameter("data(cfA5Over)") ? _request.getParameter("data(cfA5Over)").toDouble() : null
+_object.cfA6Over = _request.getParameter("data(cfA6Over)") ? _request.getParameter("data(cfA6Over)").toDouble() : null
+_object.cfA7Over = _request.getParameter("data(cfA7Over)") ? _request.getParameter("data(cfA7Over)").toDouble() : null
+_object.cfA8Over = _request.getParameter("data(cfA8Over)") ? _request.getParameter("data(cfA8Over)").toDouble() : null
+_object.cfA9Over = _request.getParameter("data(cfA9Over)") ? _request.getParameter("data(cfA9Over)").toDouble() : null
+_object.cfA10Over = _request.getParameter("data(cfA10Over)") ? _request.getParameter("data(cfA10Over)").toDouble() : null
+_object.cfA11Over = _request.getParameter("data(cfA11Over)") ? _request.getParameter("data(cfA11Over)").toDouble() : null
+
+_object.cfTotActual = _request.getParameter("data(cfTotActual)") ? _request.getParameter("data(cfTotActual)").toDouble() : null
+_object.cfTotLegal = _request.getParameter("data(cfTotLegal)") ? _request.getParameter("data(cfTotLegal)").toDouble() : null
+_object.cfTotOver = _request.getParameter("data(cfTotOver)") ? _request.getParameter("data(cfTotOver)").toDouble() : null
+
+_object.saveOrUpdateAndExecuteEntityRules()
+DomainObject.clearCache()
+
+
